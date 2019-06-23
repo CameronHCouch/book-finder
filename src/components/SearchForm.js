bb5b143fddf33004d2de5fba44b267c01bea7e26
CameronHCouch <cameronhcouch@gmail.com>
2019-06-23 12:12:00 -0400

    Add additional search parameters to form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,19 +7,23 @@ class SearchForm extends Component {
     super(props);
     this.state = {
       query: '',
+      queryLength: 10,
+      familyFriendly: true,
       books: [],
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleFamilyFriendly = this.toggleFamilyFriendly.bind(this);
   }
 
   handleChange(e) {
-    this.setState({ query: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    console.log(this.state)
     if (this.state.query) {
       this.setState( { books: [] });
       googleBooksAPIUtil(this.state.query).then(res => console.log(res))
@@ -28,12 +32,28 @@ class SearchForm extends Component {
     // else raise error
   }
 
+  toggleFamilyFriendly() {
+    this.setState({ familyFriendly: !this.state.familyFriendly })
+  }
+
   render() {
     return (
       <div className="SearchForm-wrapper">
         <form onSubmit= { this.handleSubmit }>
-          <label htmlFor="book-search">Type your book search here:</label>
-          <input type="text" value={ this.state.query } onChange= { this.handleChange } name="book-search" id="book-search"></input>
+          <label htmlFor="query">Type your book search here:</label>
+          <input type="text" value={ this.state.query } onChange= { this.handleChange } name="query" id="book-search"></input>
+          <label htmlFor="queryLength">Max Num Results</label>
+          <select name="queryLength" onChange = { this.handleChange }>
+            <option value="10" defaultValue>10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+            <option value="25">25</option>
+            <option value="30">30</option>
+            <option value="35">35</option>
+            <option value="40">40</option>
+          </select>
+          <label htmlFor="familyFriendly">Allow mature results?</label>
+          <input type="checkbox" onChange= { this.toggleFamilyFriendly } name="familyFriendly" id="familyFriendly"></input>
           <input type="submit" value="Search" id="submit"></input>
         </form>
       </div>
