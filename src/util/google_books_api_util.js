4cbf921fbb85424a477f36eb9c299c74ee640925
CameronHCouch <cameronhcouch@gmail.com>
2019-06-24 13:34:01 -0400

    Add Google Books API key to Heroku config and add to google_books_api_util.js

--- src/util/google_books_api_util.js
+++ src/util/google_books_api_util.js
@@ -1,12 +1,11 @@
-import { secretAPIKey } from './secretAPIKey';
+const PREVIEW_IMAGE = 'https://books.google.com/books/content?id=8nXLwSG2O8AC&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE7134GoVrTK0LrRho1N0mRgQpM5rzKdXsLn3-htNDnUQ1LxiidRdQiAdsKdKgTYaGC3zQOFTAggWT28neaqF5RDOT135r0GcEe4W2Sp1ZLUYf0GMUhd77QIz9T6wIwcVuUeIcSuD';
 
 export const googleBooksAPIUtil = (query, maxResults, familyFriendly) => {
-  console.log(query, familyFriendly, familyFriendly ? "not-mature" : "mature" );
   return fetch(`https://www.googleapis.com/books/v1/volumes?` +
-                `q=${query}&` +
-                `maxResults=${maxResults}&` +
-                `maxAllowedMaturityRating=${familyFriendly ? "not-mature" : "mature"}&` +
-                `key=${secretAPIKey}`
+                `q=${ query }&` +
+                `maxResults=${ maxResults }&` +
+                `maxAllowedMaturityRating=${ familyFriendly ? "not-mature" : "mature" }&` +
+                `key=${ process.env.REACT_APP_SECRET_CODE }`
               )
               .then(res => res.json())
               .then(({items}) => {
@@ -14,7 +13,7 @@ export const googleBooksAPIUtil = (query, maxResults, familyFriendly) => {
                       authors: fillMissingData('Author', volumeInfo.authors),
                       title: fillMissingData('Title', volumeInfo.title), 
                       publisher: fillMissingData('Publisher', volumeInfo.publisher), 
-                      thumbnail: volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : 'Preview Image', 
+                      thumbnail: volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : PREVIEW_IMAGE, 
                       infoLink: fillMissingData('Link', volumeInfo.infoLink)
                     })
                   )
