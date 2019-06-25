3c8298a69b4ad5a9a1d829a37ee9a81c1f81d7be
CameronHCouch <cameronhcouch@gmail.com>
2019-06-25 12:37:33 -0400

    Reformat BookListItem components

--- src/components/BookListItem.js
+++ src/components/BookListItem.js
@@ -13,11 +13,10 @@ function BookListItem(props) {
     return (
       <div className="BookListItem">
         <ul className="BookListItem-list">
-          <li><span>Title:</span> { props.title }</li>
+          <li><a href={ props.infoLink } target='_blank' rel='noopener noreferrer'> { props.title }</a></li>
           <li><span>{props.authors.length > 1 ? 'Authors' : 'Author'}:</span> { spreadAuthors() }</li>
           <li><span>Publisher:</span> { props.publisher }</li>
           <li><a href={ props.infoLink } target='_blank' rel='noopener noreferrer'><img src={ props.thumbnail } alt={`Book cover for ${props.title}`} /></a></li>
-          <li><a href={ props.infoLink } target='_blank' rel='noopener noreferrer'>Learn more!</a></li>
         </ul>
       </div>
     );
